911386be1a361d45a2473dca35d349560e60965d
saksham345 <kumarsaksham601@gmail.com>
2022-12-10 15:04:32 +0530

    made navbar dynamic

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = (props) => {
             <div style={styles.nav}>
                 <div style={styles.cartIconContainer}>
                     <img style={styles.cartIcon} src="https://cdn-icons-png.flaticon.com/128/1170/1170678.png" />
-                    <span style={styles.cartCount}>9</span>
+                    <span style={styles.cartCount}>{props.count}</span>
                 </div>
             </div>
         );
