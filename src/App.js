f5d7003d83336b9bc24f5870e3945570f367f739
saksham345 <kumarsaksham601@gmail.com>
2022-12-10 15:33:11 +0530

    added total price

--- src/App.js
+++ src/App.js
@@ -11,22 +11,22 @@ class App extends React.Component {
             {
                 price : 10000,
                 title : 'Phone',
-                qty : 20,
-                img : "",
+                qty : 2,
+                img : "https://m.media-amazon.com/images/I/41vVavfyzxS._SX300_SY300_QL70_FMwebp_.jpg",
                 id : 1
             } , 
             {
                 price : 2000,
-                title : 'Wath',
-                qty : 50,
-                img : "",
+                title : 'Watch',
+                qty : 5,
+                img : "https://images.samsung.com/is/image/samsung/p6pim/in/2108/gallery/in-galaxy-watch4-398806-sm-r865fzkainu-481079662?$730_584_PNG$",
                 id : 2
             } , 
             {
                 price : 60000,
                 title : 'Laptop',
-                qty : 10,
-                img : "",
+                qty : 6,
+                img : "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1663415676/Croma%20Assets/Computers%20Peripherals/Laptop/Images/245232_0_pin6ch.png/mxw_2048,f_auto",
                 id : 3
             }
         ]
@@ -79,6 +79,17 @@ class App extends React.Component {
     return count;
   }
 
+  getTotal = () => {
+    let {products} = this.state;
+    let total = 0;
+
+    products.forEach( (item) => {
+      total = total + (item.qty * item.price);
+    })
+
+    return total;
+  }
+
   render(){
     const {products} = this.state
     return (
@@ -90,6 +101,7 @@ class App extends React.Component {
           handleDecreaseQuantity={this.handleDecreaseQuantity} 
           handleDeleteProduct={this.handleDeleteProduct} 
         />
+        <div style={{fontSize:"25px" , padding:"20px"}}>TOTAL PRICE : {this.getTotal()}</div>
       </div>
     );
   }
